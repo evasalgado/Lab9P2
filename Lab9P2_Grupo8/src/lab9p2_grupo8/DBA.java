4c4f5724e18b18a5e635c31fec144fa197129a29
WalterjDm <walterjdoblado569@unitec.edu>
2023-09-15 14:10:22 -0600

    librerias importadas

--- Lab9P2_Grupo8/src/lab9p2_grupo8/DBA.java
+++ Lab9P2_Grupo8/src/lab9p2_grupo8/DBA.java
@@ -13,41 +13,42 @@ import java.sql.Statement;
  * @author walter
  */
 public class DBA {
+
     private String base_datos;
     private Connection dbcon;
     public Statement query;
-    
-    public DBA(String base_datos){
-        this.base_datos=base_datos;
+
+    public DBA(String base_datos) {
+        this.base_datos = base_datos;
     }
 
     public void setBase_datos(String base_datos) {
         this.base_datos = base_datos;
-    }        
-    
-   public void conectar(){
+    }
+
+    public void conectar() {
         try {
             String driver = "jdbc:ucanaccess://";
-            dbcon = DriverManager.getConnection(driver+base_datos,"","");
+            dbcon = DriverManager.getConnection(driver + base_datos, "", "");
             query = dbcon.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-   
-   public void desconectar() {
+
+    public void desconectar() {
         try {
             query.close();
             dbcon.close();
         } catch (Exception e) {
         }
     }
-   
-   public void commit(){
-       try{
-           dbcon.commit();
-       }catch(Exception e){
-           e.printStackTrace();
-       }
-   }
+
+    public void commit() {
+        try {
+            dbcon.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
